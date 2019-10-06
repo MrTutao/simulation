28256a4e95dc3349cc47456aca6d69c57e187142
tt.tu <tt.tu@ctrip.com>
2019-10-06 16:11:22 +0800

    add test module

---
+++ src/test/java/org/personal/simulation/AllTests.java
@@ -0,0 +1,16 @@
+package org.personal.simulation;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * @author taotaotu
+ * Oct 6, 2019
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses(value = {
+        AppTest.class
+})
+public class AllTests {
+
+}
---
+++ src/test/java/org/personal/simulation/AppTest.java
@@ -0,0 +1,15 @@
+package org.personal.simulation;
+
+import org.junit.Test;
+
+/**
+ * @author taotaotu
+ * Oct 6, 2019
+ */
+public class AppTest {
+
+    @Test
+    public void test() {
+
+    }
+}
